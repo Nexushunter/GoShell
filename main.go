c96838d6ad69e111f6bb3ec1ece7859504d5c858
Hunter Breathat <breathat.hunter@gmail.com>
2018-06-04 22:11:07 -0400

    Completely rewrote the main.go file as I was having issues with the termination of the background process terminating the main.go program so I made it platform agnostic, more or less, and it also cured my issue. Also added the custom cd command as the build in shell command wasn't working for me, nor will it. I have added many of the basic shortcuts and made them a bit more preferential, in my opinion. Made it so the $HOME was shortened to ~/.

--- main.go
+++ main.go
@@ -3,68 +3,138 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"os/user"
 	"strings"
-	"log"
 )
 
 // @author Hunter Breathat
-// @License MIT License
-// @repo	NexisHunter/go/src/shell
+// @License Copyright (R) 2018 Hunter Breathat
+// @repo	NexisHunter/GoShell
 /*	Objective: To create a go based shell for unix based oses
 	Currently being tested on Debian based OSes ie. Ubuntu 18.04, Kali Linux
 	Thus allowing for scripting using go as well as bash etc.*/
 
-func main(){
-	var command string
-	var input = bufio.NewReader(os.Stdin)
-	var currUser, _ = user.Current()
+var (
+	currUser, _ = user.Current()
+)
+
+func main() {
+	var command string                    // To be user input
+	var input = bufio.NewReader(os.Stdin) // Takes user input
+	//var currUser, _ = user.Current()										// Getting the current user
+	var newDir string   // New Directory
+	var err error       // Generic error
+	var cmd *os.Process // The process of the command
 
 	for {
-		fmt.Printf("%s@NexisOs> ", currUser.Username)
+		var currDir string
 
-		command,_= input.ReadString('\n')
-		command = strings.TrimSuffix(command,"\n")
-
-		commands := strings.Split(command, " ")
-		cmd,err := execute(commands)
-		if cmd != nil {
-			cmd.Wait()
-			cmd.Kill()
-		}
-		if err != nil {
-			//Display the errors
-			fmt.Println(err)
-			//Log the error
-			log.Fatal(err)
+		if currDir == "" && newDir == "" { // Checking if WD is empty
+			currDir, _ = cd("~") // Default to $HOME
+		} else {
+			currDir = newDir // Else set to new
 		}
 
+		if strings.Contains(currDir, currUser.HomeDir) {
+			currDir = strings.Replace(currDir, currUser.HomeDir, "~", -1)
+		}
+		fmt.Printf("%s@NexisOs: %s> ", currUser.Username,
+			strings.Replace(currDir, currUser.HomeDir, "~", -1)) // Main line in terminal
+
+		command, _ = input.ReadString('\n')         // Takes user input checking for \n
+		command = strings.TrimSuffix(command, "\n") // Removes trailing \n from the user input
+		commands := strings.Split(command, " ")     // Separates args from command/application
+		if commands[0] == "cd" ||                   // Checking for sudo
+			(commands[0] == "sudo" && commands[1] == "cd") {
+			if commands[1] != "cd" {
+				newDir, err = cd(commands[1])
+				if err != nil {
+					printErr(err)
+				}
+
+			} else {
+				newDir, err = cd(commands[2])
+				if err != nil {
+					printErr(err)
+				}
+			}
+		} else {
+			cmd, err = execute(commands)
+
+			if cmd != nil { // Active command/process
+				cmd.Wait() // Wait for command to finish
+				cmd.Kill() // Terminate the program after completion
+			}
+
+			if err != nil {
+				printErr(err)  // Displays the error to the user
+				log.Fatal(err) // Log the error
+			}
+		}
 	}
 }
 
+//	Os agnostic approach to launching a process
 func execute(command []string) (p *os.Process, err error) {
-
 	if binary, err := exec.LookPath(command[0]); err == nil {
-		//Set up ProcAttr
-		var procAttr os.ProcAttr
-		procAttr.Files = []*os.File{os.Stdin, os.Stdout, os.Stderr}
-		//Fork and execute command
-		p, err := os.StartProcess(binary, command, &procAttr)
+		var attributes os.ProcAttr                                    // Set up Process Attributes
+		attributes.Files = []*os.File{os.Stdin, os.Stdout, os.Stderr} // Setting IO and Error
+		p, err := os.StartProcess(binary, command, &attributes)       // Fork and execute command
 		if err == nil {
-			return p, nil
+			return p, nil // Successful execution
 		}
 	}
-	return nil, err
+	return nil, err // Fail and Error reporting
 }
 
+// Change working directory
+// Precondition: user wants to change current working directory
+// Postcondition: Either changes working directory, or reports the error
+func cd(filePath string) (fP string, err error) {
+	root := "/"                                            // root directory
+	home := "~"                                            // home directory
+	quickAcess := []string{"Desktop", "Music", "Pictures", // Quick access to ...
+		"Videos", "Documents"}
 
+	if filePath == root {
+		err := os.Chdir(root) // cd into root
+		return updateDir(fP, err)
+	} else if filePath == home {
+		err := os.Chdir(currUser.HomeDir) // cd $HOME
+		return updateDir(fP, err)
+	} else if len(filePath) > 1 && strings.Contains(filePath, "~") {
+		filePath = strings.Replace(filePath, "~", currUser.HomeDir, -1) // Allows for ~ usage
+		err := os.Chdir(filePath)
+		return updateDir(fP, err)
+	} else {
+	Outer:
+		for i := 0; i < len(quickAcess); i++ {
+			if strings.Contains(filePath, quickAcess[i]) &&
+				len(filePath) == len(quickAcess[i]) {
+				filePath = currUser.HomeDir + "/" + quickAcess[i] // Allows for cd Desktop
+				break Outer
+			}
+		}
+		err := os.Chdir(filePath)
+		return updateDir(fP, err)
+	}
+}
 
-//child_pid = syscall.SYS_FORK
-/*if child_pid  == 0 {
-	execvp(command[0],command)
-	fmt.Print("You shouldn't see me.....")
-} else {
-	waitpid(child_pid,&static_loc,syscall.WUNTRACED)
-}*/
+// func cd helper method
+func updateDir(fP string, err error) (filePath string, nah error) {
+	if err != nil {
+		fP, _ := os.Getwd() // Doesn't change working directory
+		return fP, err      // Reports the error
+	} else {
+		fP, _ := os.Getwd() // Get the new working directory
+		return fP, nil      // No error
+	}
+}
+
+// Simply Prints the error
+func printErr(err error) {
+	fmt.Println(err)
+}
